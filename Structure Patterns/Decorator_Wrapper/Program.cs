a0377449089e19b7b20718ef5597778d5a5f538a
DESKTOP-J76PCRA\Hoang <hoangnguyenle.hnlhut@gmail.com>
2023-10-31 15:30:22 +0700

    add Decorator and Facade Pattern

---
+++ Structure Patterns/Decorator_Wrapper/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace Decorator_Wrapper
+{
+    public class Program
+    {
+        public abstract class MailServiceComponent
+        {
+            public abstract bool SendMail(string message);
+        }
+
+        public class OnPremiseMailService : MailServiceComponent
+        {
+            public override bool SendMail(string message)
+            {
+                Console.WriteLine($"Send Email on {nameof(OnPremiseMailService)} with messages: {message}");
+                return true;
+            }
+        }
+
+        public class CloudMailService : MailServiceComponent
+        {
+            public override bool SendMail(string message)
+            {
+                Console.WriteLine($"Send Email on {nameof(CloudMailService)} with message {message}");
+                return true;
+            }
+        }
+
+        public abstract class MailServiceDecoratorBase : MailServiceComponent
+        {
+            protected MailServiceComponent _iMailServiceComponent;
+            public MailServiceDecoratorBase(MailServiceComponent iMailServiceComponent)
+            {
+                _iMailServiceComponent = iMailServiceComponent;
+            }
+            public override bool SendMail(string message)
+            {
+                _iMailServiceComponent.SendMail(message);
+                return true;
+            }
+        }
+
+        public class StatisticDecorator : MailServiceDecoratorBase
+        {
+            public StatisticDecorator(MailServiceComponent iMailServiceComponent) : base(iMailServiceComponent)
+            {
+            }
+
+            public override bool SendMail(string message)
+            {
+                Console.WriteLine("Calculating to get statistic");
+                base.SendMail(message);
+                return true;
+            }
+        }
+
+        public class MessageDatabaseDecorator : MailServiceDecoratorBase
+        {
+            public List<string> Messages { get; private set; } = new();
+            public MessageDatabaseDecorator(MailServiceComponent iMailServiceComponent) : base(iMailServiceComponent)
+            {
+            }
+
+            public override bool SendMail(string message)
+            {
+                if (base.SendMail(message))
+                {
+                    Messages.Add(message);  
+                    return true;
+                }
+                return false;
+                
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            OnPremiseMailService premiseMailService = new OnPremiseMailService();
+            premiseMailService.SendMail("Hi there");
+
+            CloudMailService cloudMailService = new CloudMailService();
+            cloudMailService.SendMail("Hi there");
+
+            MessageDatabaseDecorator decorator2 = new MessageDatabaseDecorator(premiseMailService);
+            decorator2.SendMail("Hi There 1");
+            decorator2.SendMail("Hi There 2");
+            decorator2.SendMail("Hi There 3");
+
+            foreach (var item in decorator2.Messages)
+            {
+                Console.WriteLine($"Saved Message: {item}");
+            }
+
+        }
+    }
+}
---
+++ Structure Patterns/Facade/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Facade
+{
+    internal class Program
+    {
+        /// <summary>
+        /// OrderService
+        /// </summary>
+        /// <param name="args"></param>
+        public class OrderService
+        {
+            public bool IsEnoughOrder(int customerId)
+            {
+                return customerId > 8 ? true : false;
+            }
+        }
+
+        public class CustomerDiscountBaseService
+        {
+            public double CalculateDiscountBase(int customerId)
+            {
+                return customerId > 15 ? 20 : 10;
+            }
+        }
+
+        public class Facade
+        {
+            private readonly OrderService _orderService;
+            private readonly CustomerDiscountBaseService _customerDiscountBaseService;
+
+            public Facade(OrderService orderService, CustomerDiscountBaseService customerDiscountBaseService)
+            {
+                _orderService = orderService;
+                _customerDiscountBaseService = customerDiscountBaseService;
+            }
+
+            public double CalculateDiscount(int customerId) 
+            {
+                if(!_orderService.IsEnoughOrder(customerId))
+                {
+                    return 0;
+                }
+                
+                return _customerDiscountBaseService.CalculateDiscountBase(customerId);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            OrderService order = new OrderService();
+            CustomerDiscountBaseService customerDis = new CustomerDiscountBaseService();
+
+            Facade facade = new Facade(order, customerDis);
+            Console.WriteLine($"Discount of Customer 1 is {facade.CalculateDiscount(1)}");
+            Console.WriteLine($"Discount of Customer 10 is {facade.CalculateDiscount(10)}");
+            Console.WriteLine($"Discount of Customer 18 is {facade.CalculateDiscount(18)}");
+
+
+        }
+    }
+}

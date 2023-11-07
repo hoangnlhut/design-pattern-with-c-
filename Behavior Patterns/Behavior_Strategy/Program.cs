815f09eddda4cce13c624c63600e224120e22920
DESKTOP-J76PCRA\Hoang <hoangnguyenle.hnlhut@gmail.com>
2023-11-07 14:46:58 +0700

    commit some code added of Strategy pattern

--- Behavior Patterns/Behavior_Strategy/Program.cs
+++ Behavior Patterns/Behavior_Strategy/Program.cs
@@ -4,73 +4,136 @@ namespace Behavior_Strategy
 {
     public class Program
     {
-        public interface ITransfortStrategy
+        #region Bài toán 1 : Vấn chuyển bằng nhiều phương thức (car, xe đạp, máy bay) qua một bộ navigator
+        //public interface ITransfortStrategy
+        //{
+        //    void Move();
+        //}
+
+        //public class Car : ITransfortStrategy
+        //{
+        //    public void Move()
+        //    {
+        //        Console.WriteLine("Moving by Car");
+        //    }
+        //}
+
+        //public class Bicycle : ITransfortStrategy
+        //{
+        //    public void Move()
+        //    {
+        //        Console.WriteLine("Moving by Bicycle");
+        //    }
+        //}
+
+
+        //public class Plane : ITransfortStrategy
+        //{
+        //    public void Move()
+        //    {
+        //        Console.WriteLine("Moving by Plane");
+        //    }
+        //}
+
+
+        //public class Navigator
+        //{
+        //    private ITransfortStrategy _transfort;
+
+        //    public void SetNavigator(ITransfortStrategy transport)
+        //    {
+        //        _transfort = transport;
+        //    }
+
+        //    public void Moving()
+        //    {
+        //        _transfort.Move();
+        //    }
+        //}
+
+
+        //static void Main(string[] args)
+        //{
+        //    Navigator navigator = new Navigator();
+
+        //    Console.WriteLine("Client choose Car transportation");
+        //    navigator.SetNavigator(new Car());
+        //    navigator.Moving();
+
+        //    Console.WriteLine("Client choose Bicycle transportation");
+        //    navigator.SetNavigator(new Bicycle());
+        //    navigator.Moving();
+
+
+        //    Console.WriteLine("Client choose Plane transportation");
+        //    navigator.SetNavigator(new Plane());
+        //    navigator.Moving();
+
+        //}
+        #endregion
+
+        #region: Bài toán 2: xuất đơn hàng bằng nhiều cách (XML, Json..) 
+        public interface IExporeService
         {
-            void Move();
+            void Export();
         }
 
-        public class Car : ITransfortStrategy
+        public class JsonExport : IExporeService
         {
-            public void Move()
+            public void Export()
             {
-                Console.WriteLine("Moving by Car");
+                Console.WriteLine("Export data to Json format!!!");
             }
         }
 
-        public class Bicycle : ITransfortStrategy
+        public class XmlExport : IExporeService
         {
-            public void Move()
+            public void Export()
             {
-                Console.WriteLine("Moving by Bicycle");
+                Console.WriteLine("Export data to Xml format!!!");
             }
         }
 
-
-        public class Plane : ITransfortStrategy
+        public class ExcelExport : IExporeService
         {
-            public void Move()
+            public void Export()
             {
-                Console.WriteLine("Moving by Plane");
+                Console.WriteLine("Export data to Excel format!!!");
             }
         }
 
-
-        public class Navigator
+        public class Orders
         {
-            private ITransfortStrategy _transfort;
-            public Navigator()
+            private IExporeService _exportService;
+            public Orders(IExporeService exportService)
             {
+                _exportService = exportService;
             }
 
-            public void SetNavigator(ITransfortStrategy transport)
+            public void GetDataToWantedFormat()
             {
-                _transfort = transport;
-            }
-
-            public void Moving()
-            {
-                _transfort.Move();
+                Console.WriteLine("Process to get data from database....");
+                _exportService.Export();
             }
         }
-
 
         static void Main(string[] args)
         {
-            Navigator navigator = new Navigator();
+            Console.WriteLine("Process and Export orders data to XML");
+            Orders order1 = new Orders(new XmlExport());
+            order1.GetDataToWantedFormat();
+            Console.WriteLine("-------------------------------------");
 
-            Console.WriteLine("Client choose Car transportation");
-            navigator.SetNavigator(new Car());
-            navigator.Moving();
+            Console.WriteLine("Process and Export orders data to JSON");
+            Orders order2 = new Orders(new JsonExport());
+            order2.GetDataToWantedFormat();
+            Console.WriteLine("-------------------------------------");
 
-            Console.WriteLine("Client choose Bicycle transportation");
-            navigator.SetNavigator(new Bicycle());
-            navigator.Moving();
-
-
-            Console.WriteLine("Client choose Plane transportation");
-            navigator.SetNavigator(new Plane());
-            navigator.Moving();
-
+            Console.WriteLine("Process and Export orders data to Excel file");
+            Orders order3 = new Orders(new ExcelExport());
+            order3.GetDataToWantedFormat();
+            Console.WriteLine("-------------------------------------");
         }
+        #endregion
     }
 }
